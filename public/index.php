4f2903851de48216a041eca33cb902f3ef489fb2
swuuws <swuuws@yeah.net>
2020-02-26 22:19:24 +0800

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Project: swuuws
+ * Producer: swuuws [ http://www.swuuws.com ]
+ * Author: A.J <804644245@qq.com>
+ * License: MIT
+ * Copyright: http://www.swuuws.com All rights reserved.
+ */
+echo 'Hello World!';
