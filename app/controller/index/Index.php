8766b3d88b5c42447715a1df66dba2e3c1939c74
swuuws <swuuws@yeah.net>
2020-05-12 11:12:50 +0800

    Create Index.php

---
+++ app/controller/index/Index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Project: swuuws
+ * Producer: swuuws [ http://www.swuuws.com ]
+ * Author: A.J <804644245@qq.com>
+ * Copyright: http://www.swuuws.com All rights reserved.
+ */
+namespace app\index;
+
+use swuuws\View;
+
+class Index
+{
+    public function index()
+    {
+        return View::view();
+    }
+}
