667e9834818b3b52efa10ce76eb0ccd16bc2ca30
swuuws <swuuws@yeah.net>
2020-05-12 11:13:00 +0800

    Create web.php

---
+++ app/route/web.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Project: swuuws
+ * Producer: swuuws [ http://www.swuuws.com ]
+ * Author: A.J <804644245@qq.com>
+ * Copyright: http://www.swuuws.com All rights reserved.
+ */
+use swuuws\Route;
+
+Route::get('/', 'index/index')->alias('index');
